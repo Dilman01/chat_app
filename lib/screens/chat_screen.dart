58c0f5d6bede7ce9a0108ea2681ae66e4918b4a9
Dilman Arif <dilmanarif01@gmail.com>
2022-07-25 15:58:36 +0300

    Add Data

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -29,7 +29,11 @@ class ChatScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/$chatId/messages')
+              .add({'text': 'Hi There.'});
+        },
       ),
     );
   }
