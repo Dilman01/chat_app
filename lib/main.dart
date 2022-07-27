934ab5e79760e445d2e439022fda15312d6e36f9
Dilman Arif <dilmanarif01@gmail.com>
2022-07-27 15:45:23 +0300

    Finish

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 
+import 'screens/splash_screen.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -39,6 +41,9 @@ class MyApp extends StatelessWidget {
             home: StreamBuilder(
               stream: FirebaseAuth.instance.authStateChanges(),
               builder: (context, userSnapShot) {
+                if (userSnapShot.connectionState == ConnectionState.waiting) {
+                  return SplashScreen();
+                }
                 if (userSnapShot.hasData) {
                   return ChatScreen();
                 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,14 +1,32 @@
 // ignore_for_file: prefer_const_constructors, avoid_function_literals_in_foreach_calls, missing_return, prefer_const_literals_to_create_immutables
 
-import 'package:chat_app/security/security.dart';
 import 'package:chat_app/widgets/chat/messages.dart';
 import 'package:chat_app/widgets/chat/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void initState() {
+    super.initState();
+    final fbm = FirebaseMessaging.instance;
+    fbm.requestPermission();
+    FirebaseMessaging.onMessage.listen((message) {
+      print(message);
+      return;
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      print(message);
+      return;
+    });
+    fbm.subscribeToTopic('chat');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +34,7 @@ class ChatScreen extends StatelessWidget {
         title: Text('Flutter Chat'),
         actions: [
           DropdownButton(
+            underline: Container(),
             icon: Icon(
               Icons.more_vert,
               color: Theme.of(context).colorScheme.secondary,
